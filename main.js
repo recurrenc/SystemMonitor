3d3173db30dface12bcfc0c9fab476745ed05fb8
Sonu Kumar <sonu.patna0808@gmail.com>
2021-06-10 23:33:50 +0530

    Creating progress bar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron");
 const { Notification } = require("electron");
 
+//Notification
+
 const NOTIFICATION_TITLE = "Basic Notification";
 const NOTIFICATION_BODY = "Notification from the Main process";
 
@@ -11,6 +13,8 @@ function showNotification() {
   }).show();
 }
 
+let progressInterval;
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -18,11 +22,37 @@ function createWindow() {
   });
 
   win.loadFile("index.html");
+
+  const INCREMENT = 0.03;
+  const INTERVAL_DELAY = 100; // ms
+
+  let c = 0;
+  progressInterval = setInterval(() => {
+    // update progress bar to next value
+    // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
+    win.setProgressBar(c);
+
+    // increment or reset progress bar
+    if (c < 2) c += INCREMENT;
+    else c = 0;
+  }, INTERVAL_DELAY);
 }
 
-app
-  .whenReady()
-  .then(() => {
+app.whenReady().then(createWindow).then(showNotification);
+
+// before the app is terminated, clear both timers
+app.on("before-quit", () => {
+  clearInterval(progressInterval);
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-  })
-  .then(showNotification);
+  }
+});
